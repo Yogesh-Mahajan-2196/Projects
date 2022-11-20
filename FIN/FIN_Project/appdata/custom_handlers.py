e8929189c3e6b945574d7107604ef843cf2472ff
Yogesh-Mahajan-2196 <m.yogesh2109@gmail.com>
2022-11-20 14:59:19 +0530

    'JSON FIELD FIXED'

--- FIN/FIN_Project/appdata/custom_handlers.py
+++ FIN/FIN_Project/appdata/custom_handlers.py
@@ -1,5 +1,20 @@
 from django.http import JsonResponse
+from .model import Employee
+from .serializers import EmployeeSerializers
+
+
+def handler200(request, *args, **kwargs):
+    data = {"message": "Employee Created Successfully.",
+            "success": True, "status": 200}
+    return JsonResponse(data = data)
+
+def handler200(request, *args, **kwargs):
+    emp = Employee.objects.get
+    data = {"message": "employee already exist.",
+            "success": False, "status": 200}
+    return JsonResponse(data = data)
 
 def handler200(request, *args, **kwargs):
     data = {"message": "employee already exist.",
-            "success": False}
+        "success": False, "status": 200}
+    return JsonResponse(data = data)
--- FIN/FIN_Project/appdata/models.py
+++ FIN/FIN_Project/appdata/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
 class Employee(models.Model):
+    eid = models.AutoField()
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=100, unique=True)
     age = models.IntegerField()
     gender = models.CharField(max_length=20)
     phone_No = models.CharField(max_length=20)
-    address_Details = models.JSONField(null = True)
-    work_Experience = models.JSONField(null = True)
-    qualifications = models.JSONField(null = True)
-    projects = models.JSONField(null = True)
+    address_Details = models.JSONField(default = {"hno": "null", "street": "null", "city": "null", "state": "null"})
+    work_Experience = models.JSONField(default = {"comapny_Name": "null", "fromData": "null", "to_Data": "null", "address": "null"})
+    qualifications = models.JSONField(default = {"qualification_Name": "null", "percentage": "null", "projects": "null"})
+    projects = models.JSONField(default = {"title": "null", "description": "null"})
     photo = models.ImageField(upload_to = "images", null = True, blank = True)
 
     def __str__(self):
